8798be4ec84fe46cf7949d24cc5d5d9c4e0570e2
Michal Kus <michal513717@gmail.com>
2025-01-07 21:35:23 +0100

    Small changes

---
+++ src/utils/events.ts
@@ -0,0 +1,20 @@
+import { ProductEvent } from "../models/common.models";
+
+export class EventsCreator<T extends ProductEvent> {
+    private data: Omit<T, 'type' | 'timestamp' | 'status' | 'error'>;
+    private type: T['type'];
+
+    constructor(type: T['type'], data: Omit<T, 'type' | 'timestamp' | 'status' | 'error'>) {
+        this.type = type;
+        this.data = data;
+    }
+
+    create(): T {
+        return {
+            type: this.type,
+            timestamp: new Date(),
+            ...this.data,
+            status: "SUCCESS"
+        } as T;
+    }
+}
--- src/utils/responses.ts
+++ src/utils/responses.ts
@@ -3,8 +3,16 @@ import type { Response } from "express";
 
 export function internalServerErrorResponse(res: Response) {
     return res.status(500).json({
-        status: ResponseStatus.SUCCESS,
+        status: ResponseStatus.FAILED,
         code: ERROR_CODES.INTERNAL_SERVER_ERROR,
         message: "Internal Server Error, please try again later"
+    })
+}
+
+export function validationErrorResponse(res: Response, data: any) { 
+    return res.status(403).json({
+        status: ResponseStatus.FAILED,
+        code: ERROR_CODES.VALIDATION_ERROR,
+        message: data
     })
 }
